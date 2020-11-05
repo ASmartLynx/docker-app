3ef42952b15b3c2397eaa679c818cde2b7f7e0f7
Aleksandr <a.kartsev93@gmail.com>
2020-11-05 12:42:38 +0300

    Database service

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { connectDb } = require('./helpers/db');
+const { host, port, db } = require('./configuration');
 const app = express();
-const port = process.env.PORT;
-const host = process.env.HOST;
+
+const postSchema = new mongoose.Schema({
+  name: String
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+const startServer = () => {
+  app.listen(port || 3000, () => {
+    console.log(`Started on port ${port}`);
+    console.log(`Started on host ${host}`);
+    console.log(`Our database ${db}`);
+
+    const silence = new Post({ name: 'Silence' });
+    silence.save(function(err, savedSilcence) {
+      if (err) return console.log(err);
+      console.log('savedSilence with volumes', savedSilcence);
+    });
+  });
+}
 
 app.get('/test', (req, res) => {
   res.send('Ok');
 });
 
-app.listen(port || 3000, () => {
-  console.log(`Started on port ${port}`);
-  console.log(`Started on host ${host}`);
-});
+connectDb()
+  .on('error', console.log)
+  .on('disconnected', connectDb)
+  .once('open', startServer);
